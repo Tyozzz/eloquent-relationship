ce62f8f48bdda40148c326e9b3db5b0a9fdac4b8
Tyozzz <satrionugroho182008@gmail.com>
2025-01-15 18:41:59 +0700

    Membuat Relasi Many to Many

--- database/migrations/2025_01_15_113436_create_roles_table.php
+++ database/migrations/2025_01_15_113436_create_roles_table.php
@@ -1,4 +1,17 @@
-public function up()
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
 {
     Schema::create('roles', function (Blueprint $table) {
         $table->id();
@@ -6,3 +19,14 @@ public function up()
         $table->timestamps();
     });
 }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
