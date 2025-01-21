8a67ab913bd609c9cbd89d1503e6fc835a48b8e0
Tyozzz <satrionugroho182008@gmail.com>
2025-01-21 09:53:23 +0700

    Perubahan

--- database/migrations/2025_01_15_105318_create_posts_table.php
+++ database/migrations/2025_01_15_105318_create_posts_table.php
@@ -1,9 +1,32 @@
-public function up()
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('posts', function (Blueprint $table) {
-        $table->id();
-        $table->string('title');
-        $table->text('content');
-        $table->timestamps();
-    });
-}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- database/migrations/2025_01_15_111358_create_comments_table.php
+++ database/migrations/2025_01_15_111358_create_comments_table.php
@@ -12,12 +12,16 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('comments', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('post_id');
+        $table->text('comment');
+        $table->timestamps();
+
+        $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+    });
+}
 
     /**
      * Reverse the migrations.
